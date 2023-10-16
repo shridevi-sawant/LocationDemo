29244a4b3e1230e9bd327125a136f4c191475ce2
shridevi-sawant <shridevi.hiroji@gmail.com>
2023-10-16 09:57:06 +0530

    feature #1

--- LocationDemo/ViewController.swift
+++ LocationDemo/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("View did load")
     }
 
     @IBAction func startClick(_ sender: Any) {
