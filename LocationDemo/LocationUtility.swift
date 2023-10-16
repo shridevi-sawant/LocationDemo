6d21f3f8f5d02b688f465e2e3538acccd93f365e
shridevi-sawant <shridevi.hiroji@gmail.com>
2023-10-16 09:47:53 +0530

    Initial Commit

---
+++ LocationDemo/LocationUtility.swift
@@ -0,0 +1,77 @@
+//
+//  LocationUtility.swift
+//  LocationDemo
+//
+//  Created by Shridevi Sawant on 13/10/23.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationUtility: NSObject {
+    
+    let locManager = CLLocationManager()
+    var isAuthorized = false
+    var currentLocation: CLLocation?
+    
+    override init() {
+        super.init()
+        locManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locManager.distanceFilter = 10 // meters
+        locManager.requestWhenInUseAuthorization()
+        locManager.delegate = self
+    }
+    
+    func startTracking() {
+        print("Tracking started")
+        locManager.startUpdatingLocation()
+    }
+    
+    func stopTracking() {
+        locManager.stopUpdatingLocation()
+        print("Tracking stopped")
+    }
+    
+    func getAddress(callback: @escaping (String) -> Void) {
+        let gc = CLGeocoder()
+        var address = ""
+        if let loc = currentLocation {
+            gc.reverseGeocodeLocation(loc) { places, _ in
+                
+                if let foundPlaces = places {
+                    let place = foundPlaces[0]
+                    address = """
+                        \(place.subLocality ?? "")
+                        \(place.locality ?? "")
+                        \(place.administrativeArea ?? ""), \(place.postalCode ?? "")
+                        \(place.country ?? "")
+                    """
+                    callback(address)
+                }
+            }
+        }
+    }
+}
+
+extension LocationUtility: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let loc = locations.last {
+            print("didUpdateLocations: \(loc.coordinate.latitude), \(loc.coordinate.longitude)")
+            currentLocation = loc
+        }
+        
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse:
+            isAuthorized = true
+        case .authorizedAlways:
+            isAuthorized = true
+        default:
+            isAuthorized = false
+        }
+    }
+}
---
+++ LocationDemo/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  LocationDemo
+//
+//  Created by Shridevi Sawant on 13/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var statusL: UILabel!
+    
+    let locUtility = LocationUtility()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func startClick(_ sender: Any) {
+        if locUtility.isAuthorized {
+            locUtility.startTracking()
+            statusL.text = "Tracking started.."
+        }
+        else {
+            print("Authorization is not avaialable")
+        }
+    }
+    
+    // url scheme
+    /*
+     http - browser
+     geo - map
+     mailto - email
+     smsto - messeging
+     tel - call
+     */
+    @IBAction func mapClick(_ sender: Any) {
+        // open map application
+        if let loc = locUtility.currentLocation {
+            let mapUrl = "http://maps.apple.com/?ll=\(loc.coordinate.latitude),\(loc.coordinate.longitude)"
+            
+            if let mUrl = URL(string: mapUrl)
+            {
+                if UIApplication.shared.canOpenURL(mUrl) {
+                    UIApplication.shared.open(mUrl)
+                }
+                else {
+                    print("Map application not found")
+                }
+            }
+        }
+    }
+    
+    @IBAction func addressClick(_ sender: Any) {
+        locUtility.getAddress { addr in
+            self.statusL.text = addr
+        }
+    }
+    
+    @IBAction func currentClick(_ sender: Any) {
+        if let loc = locUtility.currentLocation {
+            statusL.text = """
+            Lattitude: \(loc.coordinate.latitude)
+            Longitude: \(loc.coordinate.longitude)
+            """
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        locUtility.stopTracking()
+    }
+}
+
